c68314bd21c24d44011d930537471411a4eb4b81
Caspar Nettelbladt <caspar.nettelbladt@sectra.com>
2018-03-03 17:37:13 +0100

    Removed old implementation of the hash map in the point and replaced with the compared

--- RobotCleaner/RobotCleaner/Point.cs
+++ RobotCleaner/RobotCleaner/Point.cs
@@ -1,5 +1,4 @@
 ﻿namespace RobotCleaner {
-//HashSet<Point> list = new HashSet<Point>(new PointComparer());
     public class Point {
         public Point(int x, int y) {
             X = x;
@@ -9,25 +8,5 @@
         public int X { get; set; }
 
         public int Y { get; set; }
-//class PointComparer : IEqualityComparer<Point> {
-//    public bool Equals(Point x, Point y) {
-//        return x.X == y.X && x.Y == y.Y;
-//    }
-
-//    public int GetHashCode(Point obj) {
-//        // Perfect hash for practical bitmaps, their width/height is never >= 65536
-//        return (obj.Y << 16) ^ obj.X;
-//    }
-//}
-        public override int GetHashCode() {
-            unchecked {
-                int hash = 17;
-
-                // Suitable nullity checks etc, of course :)
-                hash = hash * 23 + X.GetHashCode();
-                hash = hash * 23 + Y.GetHashCode();
-                return hash;
-            }
-        }
     }
 }
