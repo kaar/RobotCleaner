ce7555ad6b3cde4c75bc542e879e9ed14c2a9409
Caspar Nettelbladt <caspar.nettelbladt@sectra.com>
2018-03-04 09:11:04 +0100

    Passnig test for Build East

--- RobotCleaner/RobotCleaner.Tests/CommandBuilderTests.cs
+++ RobotCleaner/RobotCleaner.Tests/CommandBuilderTests.cs
@@ -3,14 +3,22 @@
 namespace RobotCleaner.Tests {
     [TestFixture]
     public class CommandBuilderTests {
+        private CommandBuilder commandBuilder;
+
+        [SetUp]
+        public void SetUp() {
+            commandBuilder = new CommandBuilder();
+        }
 
         [Test]
-        public void Test() {
+        public void Create_DirectionEast_BuildEastCommand() {
             // Arrange
-            // Act
 
+            // Act
+            ICommand actual = commandBuilder.Create("E", 10);
 
             // Assert
+            Assert.That(actual, Is.TypeOf(typeof(East)));
         }
     }
 }
--- RobotCleaner/RobotCleaner/Command.cs
+++ RobotCleaner/RobotCleaner/Command.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-
+﻿
 namespace RobotCleaner {
     public class Command {
         private readonly string direction;
@@ -18,16 +17,29 @@ namespace RobotCleaner {
         }
     }
 
-    public class CommandBuild {
-        public Command Create(string direction, int steps) {
-            if direction == 
-            return new Command(directionm);
+    public interface ICommand {
+        Point Execute(Point start);
+    }
+
+    public class CommandBuilder {
+        public ICommand Create(string direction, int steps) {
+            if (direction == "E") {
+                return new East(steps);
+            }
+
+            return null;
         }
     }
 
-    public class East : Command {
-        public East (int steps) : base(steps) {
-            
+    public class East : ICommand {
+        public East(int steps) {
+            Steps = steps;
+        }
+
+        public int Steps { get; }
+
+        public Point Execute(Point start) {
+            return new Point(start.X + Steps, start.Y);
         }
     }
 }
