f0105e0b38f57b73e4ab3b3521bc18614f04c788
Caspar Nettelbladt <caspar.nettelbladt@sectra.com>
2018-03-04 17:56:58 +0100

    Added a TestCase for Program.Main. Didn't know you could. :)

---
+++ RobotCleaner/RobotCleaner.Tests/ProgramTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace RobotCleaner.Tests {
+    public class ProgramTests {
+        private StringWriter stringWriter;
+
+        [SetUp]
+        public void SetUp() {
+            stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+        }
+
+        [Test]
+        public void Main_WithInputExample() {
+            // Arrange
+            const string testData = "2\n10 22\nE 2\nN 1\n";
+            var sr = new StringReader(testData);
+            Console.SetIn(sr);
+
+            // Act
+            string[] args = { };
+            Program.Main(args);
+
+            // Assert
+            string result = stringWriter.ToString();
+            Console.WriteLine(result);
+            Assert.That(result, Is.EqualTo("=> Cleaned: 4\r\n"));
+        }
+    }
+}
