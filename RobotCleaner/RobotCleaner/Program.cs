cbeee0a22da256733516dd76867cb7a4e4e04c1f
Caspar Nettelbladt <caspar.nettelbladt@sectra.com>
2018-03-04 15:32:18 +0100

    Implemented Program.cs

--- RobotCleaner/RobotCleaner/Program.cs
+++ RobotCleaner/RobotCleaner/Program.cs
@@ -1,12 +1,24 @@
-﻿namespace RobotCleaner {
+﻿using System;
+using RobotCleaner.Commands;
+
+namespace RobotCleaner {
     public class Program {
         public static void Main(string[] args) {
-            int numberOfCommands = int.Parse(args[0]);
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            string startPos = Console.ReadLine();
+            var start = new Point(0, 0);
+            var room = new Room();
+            var robot = new Robot(room, start);
+            for (var i = 0; i < numberOfCommands; i++) {
+                string[] commandLine = Console.ReadLine()
+                                         ?.Split(' ');
+                string direction = commandLine[0];
+                int steps = int.Parse(commandLine[1]);
+                ICommand command = CommandFactory.Create(direction, steps);
+                robot.ExecuteCommand(command);
+            }
 
-            //var commands = new Command[numberOfCommands];
-
-            // Robot robot = new Robot(x,y);
-            Room room = new Room();
+            Console.WriteLine($"=> Cleaned: {room.CleanedSquares()}");
         }
     }
 }
