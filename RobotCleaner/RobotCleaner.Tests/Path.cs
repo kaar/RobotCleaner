a8588085955677ca61b724c962789dbf4709552d
Caspar Nettelbladt <caspar.nettelbladt@sectra.com>
2018-03-04 11:02:07 +0100

    Failing test for negative path

--- RobotCleaner/RobotCleaner.Tests/Path.cs
+++ RobotCleaner/RobotCleaner.Tests/Path.cs
@@ -37,5 +37,37 @@ namespace RobotCleaner.Tests {
             // Assert
             Assert.That(points, Is.EquivalentTo(new List<Point> { new Point(0, 0), new Point(1, 0), new Point(2, 0) }));
         }
+
+        [Test]
+        public void GetPoints_MovingNegativeXAxis_AllPoints() {
+            // Arrange
+            Point start = new Point(2, 0);
+            Point end = new Point(0, 0);
+
+            Path path = new Path(start, end);
+
+            // Act
+            List<Point> points = path.GetPoints()
+                                     .ToList();
+
+            // Assert
+            Assert.That(points, Is.EquivalentTo(new List<Point> { new Point(0, 0), new Point(1, 0), new Point(2, 0) }));
+        }
+
+        [Test]
+        public void GetPoints_MovingNegativeYAxis_AllPoints() {
+            // Arrange
+            Point start = new Point(0, 2);
+            Point end = new Point(0, 0);
+
+            Path path = new Path(start, end);
+
+            // Act
+            List<Point> points = path.GetPoints()
+                                     .ToList();
+
+            // Assert
+            Assert.That(points, Is.EquivalentTo(new List<Point> { new Point(0, 0), new Point(0, 1), new Point(0, 2) }));
+        }
     }
 }
